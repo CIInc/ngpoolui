ecd94ac380dfc0d7efc870d20e5f4560dab0b9df
Aymeric Grassart <aymeric@cidevelop.com>
2017-12-31 15:58:58 -0600

    .

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send();
+  //res.render('error');
 });
 
 module.exports = app;
