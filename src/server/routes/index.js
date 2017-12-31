515b100f9c0409cb8e4fda62af380a7adbac48e2
Aymeric Grassart <aymeric@cidevelop.com>
2017-12-31 15:51:51 -0600

    .

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router();
 //const root = './';
 
 router.get('/', function(req, res, next) {
-    res.sendFile('./dist/index.html');//, {root}
-  });
-router.get('*', function(req, res, next) {
+  res.sendFile('./dist/index.html');//, {root}
+});
+router.get('/home', function(req, res, next) {
+  res.sendFile('./dist/index.html');//, {root}
+});
+router.get('/*', function(req, res, next) {
     res.sendFile('./dist/index.html');//, {root}
   });
 
