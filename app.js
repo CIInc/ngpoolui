47b093d5fcb732cb54fc7f961b8050381ff94951
Aymeric Grassart <aymeric@cidevelop.com>
2017-12-31 12:40:40 -0600

    .

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+
 const routes = require('./src/server/routes');
 
-const root = './';
 //const port = process.env.PORT || '3000';
 const app = express();
 
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+//app.use(cookieParser());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.static(path.join(root, 'dist')));
+
 app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use('/', routes);
 app.use('/api', routes);
 app.use('*', routes);
+
 /*
 app.get('*', (req, res) => {
-  res.sendFile('../index.html', {root});
+  res.sendFile('../index.html', {'./'});
 });
 app.listen(port, () => console.log(`API running on localhost:${port}`));
 */
@@ -40,51 +54,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-/*
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-*/
