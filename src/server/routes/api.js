70a6d03e25bfb32ced4940592ce0f22d167e434d
Aymeric Grassart <aymeric@cidevelop.com>
2017-12-31 14:55:24 -0600

    .

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const poolService = require('./pool.service'); 
+const poolService = require('../pool.service'); 
 
 router.get('/heroes', (req, res) => {
  res.status(200).send([
