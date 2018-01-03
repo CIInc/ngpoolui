4531f0e7db4bf3f7c203c73122cfb5e9fbd7aeb6
Aymeric Grassart <aymeric@cidevelop.com>
2018-01-02 22:27:57 -0600

    uncommented polyfills for IE9, 10, 11

--- src/app/services/pool-api.service.ts
+++ src/app/services/pool-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
+//import { of } from 'rxjs/observable/of';
 import { forkJoin } from 'rxjs/observable/forkJoin';
 
 import { Pool } from '../models/pool';
